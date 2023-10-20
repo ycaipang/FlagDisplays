ba1c101ebec24dfc2d42a1996d37267530b84f88
Yul Caipang <ycaipang@gmail.com>
2023-10-21 00:53:55 +0800

    js and css change on product page for main category and results

--- web/themes/custom/YC_theme/js/product-filter.js
+++ web/themes/custom/YC_theme/js/product-filter.js
@@ -16,12 +16,15 @@
     Drupal.behaviors.product_filter = {
       attach: function(context, settings) {
 
-        $("#edit-fd-type--2 ul li a.bef-link--selected, #edit-fd-type--3 ul li a.bef-link--selected").next().show(300);
-        $("#edit-fd-type--2 ul li a.bef-link--selected, #edit-fd-type--3 ul li a.bef-link--selected").parents().eq(1).show();
-        $('div.Feather.Flags').hide().slice(0, 12).show();
-
-        var more_btn = $( "<div id='object1'></div>" )
-        $('div.Feather.Flags:visible:last').parents().eq(2).append("<div id='object1'>View more...</div>");
+        // show hide submenu on main filter
+        $( "#edit-fd-type--2 ul li a, #edit-fd-type--2 ul ul li a" ).each(function() {
+          if ($(this).hasClass('bef-link--selected')) {
+            $(this).next().addClass('show');
+            $(this).parents().eq(1).addClass('show');
+          } else {
+            $(this).next().removeClass('show');
+          }
+        });
 
         if ($(".view-products .view-empty")[0]) {
             $("#content .section > div:nth-child(-n+6)").show();
@@ -30,6 +33,24 @@
             $("#content .section > div:nth-child(-n+6)").hide();
             $("#block-exposedformproductspage-1-2").show();
         };
+
+
+        // add class to indentify flag category
+        $( "div.Feather.Flags" ).each(function() {
+          $( this ).parents().eq(2).addClass('feather-flags');
+        });
+
+        var queryString = window.location.search;
+        var urlParam = new URLSearchParams(queryString);
+        var prod_type = urlParam.get('fd_type');
+
+        if (prod_type == 1 && $(".view-products .view-content > div").hasClass('feather-flags')) {
+          $('div.feather-flags:lt(9)').show();
+          $('div.feather-flags:visible:last').html("<div id='more-btn'>View more...</div>");
+        } else {
+          $(".view-products .view-content > div").removeClass('feather-flags');
+        }
+        
 
       }
     };
